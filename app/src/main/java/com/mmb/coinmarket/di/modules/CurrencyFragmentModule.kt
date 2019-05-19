a8239c84988f5df338e75b8ef26b3b6a184c4a60
mary <fakemariastyles>
2019-05-19 19:38:01 +0430

    edited FragmentModule

--- app/src/main/java/com/mmb/coinmarket/di/modules/CurrencyFragmentModule.kt
+++ app/src/main/java/com/mmb/coinmarket/di/modules/CurrencyFragmentModule.kt
@@ -1,11 +1,21 @@
 package com.mmb.coinmarket.di.modules
 
-import com.mmb.coinmarket.currencyFragment.presenter.CurrencyPresenter
+import com.mmb.coinmarket.domain.executor.UseCaseExecutorObserveThread
+import com.mmb.coinmarket.domain.executor.UseCaseExecutorThread
+import com.mmb.coinmarket.domain.intractor.GetLatestUseCase
+import com.mmb.coinmarket.domain.repository.CoinMarketRepository
 import dagger.Provides
 
 class CurrencyFragmentModule {
     @Provides
-    fun provideCurrencyPresenterImpl() {
-        return CurrencyPresenter()
+    fun getLatestUseCase(
+        coinMarketRepository: CoinMarketRepository,
+        thread: UseCaseExecutorThread,
+        observeThread: UseCaseExecutorObserveThread
+    ): GetLatestUseCase {
+        return GetLatestUseCase(
+            thread = thread, observeThread = observeThread
+            , coinMarketRepository = coinMarketRepository
+        )
     }
 }
